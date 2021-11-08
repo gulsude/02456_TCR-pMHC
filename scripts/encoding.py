2a098463454d9a2266ff5277e52f60079a424bed
dayadmg <daya.dmg@gmail.com>
2021-11-08 14:37:20 +0100

    merging esm into encoding

--- scripts/encoding.py
+++ scripts/encoding.py
@@ -1,6 +1,18 @@
 # different encodings here
+# you need: pip install fair-esm
+
 import pandas as pd
 import numpy as np
+import torch
+import esm
+
+# Load pre-trained ESM-1b model
+model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+batch_converter = alphabet.get_batch_converter()
+
+# Load pre-trained ESM-MSA-1b model
+modelMSA, alphabet2 = esm.pretrained.esm_msa1b_t12_100M_UR50S()
+batch_converter = alphabet.get_batch_converter()
 
 # list of aa and list of properties in matrix aaIndex
 aminoacidTp = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
