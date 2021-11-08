2c38262cc920ddfcf17585e21631803f3fdef035
dayadmg <daya.dmg@gmail.com>
2021-11-08 14:20:32 +0100

    adding esm-MSA model to esm1b script

--- scripts/esm1b.py
+++ scripts/esm1b.py
@@ -6,3 +6,7 @@ import esm
 # Load ESM-1b model
 model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
 batch_converter = alphabet.get_batch_converter()
+
+# Load  ESM-MSA-1b model
+modelMSA, alphabet2 = esm.pretrained.esm_msa1b_t12_100M_UR50S()
+batch_converter = alphabet.get_batch_converter()
