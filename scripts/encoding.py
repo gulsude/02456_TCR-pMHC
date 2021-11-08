1514bd06b5f11429a766e15fdfb57bc084ba1ee5
gulsude <gulsudedemircan@gmail.com>
2021-11-08 15:08:30 +0100

    add esm 1b function

--- scripts/encoding.py
+++ scripts/encoding.py
@@ -6,13 +6,17 @@ import numpy as np
 import torch
 import esm
 
-# Load pre-trained ESM-1b model
-model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
-batch_converter = alphabet.get_batch_converter()
-
-# Load pre-trained ESM-MSA-1b model
-modelMSA, alphabet2 = esm.pretrained.esm_msa1b_t12_100M_UR50S()
-batch_converter = alphabet.get_batch_converter()
+def esm_1b(peptides):
+    embeddings = list()
+    # Load pre-trained ESM-1b model
+    model, alphabet = esm.pretrained.esm1b_t33_650M_UR50S()
+    batch_converter = alphabet.get_batch_converter()
+    # Load pre-trained ESM-MSA-1b model
+    modelMSA, alphabet2 = esm.pretrained.esm_msa1b_t12_100M_UR50S()
+    batch_converter = alphabet.get_batch_converter()
+    
+    return embeddings
+    
 
 # list of aa and list of properties in matrix aaIndex
 aminoacidTp = ['A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V']
