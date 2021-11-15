0442f2689d44c834e62ffeba2a2312ecd01d34e6
gulsude <gulsudedemircan@gmail.com>
2021-11-15 16:05:50 +0100

    Model from the thesis (cnn + rnn) is added

--- scripts/functions.py
+++ scripts/functions.py
@@ -3,38 +3,39 @@ import pandas as pd
 import numpy as np
 import torch
 import math
+from sklearn.metrics import accuracy_score
 
 
 def reverseOneHot(encoding):
-    """
+    “”"
     Converts one-hot encoded array back to string sequence
-    """
-    mapping = dict(zip(range(20),"ACDEFGHIKLMNPQRSTVWY"))
-    seq=''
+    “”"
+    mapping = dict(zip(range(20),“ACDEFGHIKLMNPQRSTVWY”))
+    seq=‘’
     for i in range(len(encoding)):
         if np.max(encoding[i])>0:
             seq+=mapping[np.argmax(encoding[i])]
     return seq
 
 def extract_sequences(dataset_X):
-    """
+    “”"
     Return DataFrame with MHC, peptide and TCR a/b sequences from
     one-hot encoded complex sequences in dataset X
-    """
+    “”"
     mhc_sequences = [reverseOneHot(arr[0:179,0:20]) for arr in dataset_X]
     pep_sequences = [reverseOneHot(arr[179:190,0:20]) for arr in dataset_X]
     tcr_sequences = [reverseOneHot(arr[192:,0:20]) for arr in dataset_X]
-    df_sequences = pd.DataFrame({"MHC":mhc_sequences, "peptide":pep_sequences,
-                                 "tcr":tcr_sequences})
+    df_sequences = pd.DataFrame({“MHC”:mhc_sequences, “peptide”:pep_sequences,
+                                 “tcr”:tcr_sequences})
     return df_sequences
 
 def load_peptide_target(filename):
-    """
+    “”"
     Read amino acid sequence of peptides and
     corresponding log transformed IC50 binding values from text file.
-    """
-    df = pd.read_csv(filename, sep='\s+', usecols=[0,1], names=['peptide','target'])
-    return df.sort_values(by='target', ascending=False).reset_index(drop=True)
+    “”"
+    df = pd.read_csv(filename, sep=‘\s+’, usecols=[0,1], names=[‘peptide’,‘target’])
+    return df.sort_values(by=‘target’, ascending=False).reset_index(drop=True)
 
 def invoke(early_stopping, loss, model, implement=False):
     if implement == False:
@@ -42,7 +43,7 @@ def invoke(early_stopping, loss, model, implement=False):
     else:
         early_stopping(loss, model)
         if early_stopping.early_stop:
-            #print("Early stopping")
+            #print(“Early stopping”)
             return True
 
 def count_parameters(model):
@@ -52,20 +53,20 @@ def construct_pssm(data):
     beta = 50.0
     peptides = data
     sequence_weighting = True
-    file_name = f"../data/Matrices/PSSM"
-    alphabet = np.loadtxt(f"../data/Matrices/alphabet", dtype=str)
-    _bg = np.loadtxt(f"../data/Matrices/bg.freq.fmt", dtype=float)
+    file_name = f”../data/Matrices/PSSM”
+    alphabet = np.loadtxt(f”../data/Matrices/alphabet”, dtype=str)
+    _bg = np.loadtxt(f”../data/Matrices/bg.freq.fmt”, dtype=float)
     bg = {}
     for i in range(0, len(alphabet)):
         bg[alphabet[i]] = _bg[i]
-    _blosum62 = np.loadtxt(f"../data/Matrices/blosum62.freq_rownorm", dtype=float).T
+    _blosum62 = np.loadtxt(f”../data/Matrices/blosum62.freq_rownorm”, dtype=float).T
     blosum62 = {}
 
     for i, letter_1 in enumerate(alphabet):
         blosum62[letter_1] = {}
         for j, letter_2 in enumerate(alphabet):
             blosum62[letter_1][letter_2] = _blosum62[i, j]
-            
+
     if len(peptides[0]) == 1:
         peptide_length = len(peptides)
         peptides = [peptides]
@@ -74,7 +75,7 @@ def construct_pssm(data):
 
     for i in range(0, len(peptides)):
         if len(peptides[i]) != peptide_length:
-            print("Error, peptides differ in length!")
+            print(“Error, peptides differ in length!“)
 
     #print peptides
 
@@ -82,7 +83,7 @@ def construct_pssm(data):
         init_matrix = [0]*peptide_length
         for i in range(0, peptide_length):
             row = {}
-            for letter in alphabet: 
+            for letter in alphabet:
                 row[letter] = 0.0
             #fancy way:  row = dict( zip( alphabet, [0.0]*len(alphabet) ) )
             init_matrix[i] = row
@@ -106,7 +107,7 @@ def construct_pssm(data):
             neff = 0.0
             for position in range(0, peptide_length):
                 r = 0
-                for letter in alphabet:        
+                for letter in alphabet:
                     if c_matrix[position][letter] != 0:
                         r += 1
                 s = c_matrix[position][peptide[position]]
@@ -115,8 +116,8 @@ def construct_pssm(data):
             neff = neff / peptide_length
         # do not apply sequence weighting
         else:
-            w = 1  
-            neff = len(peptides)  
+            w = 1
+            neff = len(peptides)
         weights[peptide] = w
 
 
@@ -128,7 +129,7 @@ def construct_pssm(data):
         for peptide in peptides:
             f_matrix[position][peptide[position]] += weights[peptide]
             n += weights[peptide]
-        for letter in alphabet: 
+        for letter in alphabet:
             f_matrix[position][letter] = f_matrix[position][letter]/n
 
 
@@ -136,7 +137,7 @@ def construct_pssm(data):
     g_matrix = initialize_matrix(peptide_length, alphabet)
     for position in range(0, peptide_length):
         for letter_1 in alphabet:
-            for letter_2 in alphabet:               
+            for letter_2 in alphabet:
                 g_matrix[position][letter_1] += f_matrix[position][letter_2] * blosum62[letter_1][letter_2]
 
 
@@ -154,28 +155,28 @@ def construct_pssm(data):
             if p_matrix[position][letter] > 0:
                 w_matrix[position][letter] = 2 * math.log(p_matrix[position][letter]/bg[letter])/math.log(2)
             else:
-                w_matrix[position][letter] = 0 
+                w_matrix[position][letter] = 0
 
 
     # ### Write Matrix to PSI-BLAST format
     # ### convert w_matrix to PSI-BLAST format and print to file
 
     def to_psi_blast_file(matrix, file_name):
-        with open(file_name, 'w') as file:
-            file.write("A\tR\tN\tD\tC\tQ\tE\tG\tH\tI\tL\tK\tM\tF\tP\tS\tT\tW\tY\tV\n")
-            letter_order = ["A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V"]
+        with open(file_name, ‘w’) as file:
+            file.write(“A\tR\tN\tD\tC\tQ\tE\tG\tH\tI\tL\tK\tM\tF\tP\tS\tT\tW\tY\tV\n”)
+            letter_order = [“A”, “R”, “N”, “D”, “C”, “Q”, “E”, “G”, “H”, “I”, “L”, “K”, “M”, “F”, “P”, “S”, “T”, “W”, “Y”, “V”]
             for i, row in enumerate(matrix):
                 scores = []
-                #scores.append(str(i+1) + " A")
+                #scores.append(str(i+1) + ” A”)
                 #scores.append(str(i+1))
                 for letter in letter_order:
                     score = row[letter]
                     scores.append(round(score, 4))
-                file.write(str(scores[0])+"\t"+str(scores[1])+"\t"+str(scores[2])+"\t"+str(scores[3])+"\t"+
-                          str(scores[4])+"\t"+str(scores[5])+"\t"+str(scores[6])+"\t"+str(scores[7])+"\t"+
-                          str(scores[8])+"\t"+str(scores[9])+"\t"+str(scores[10])+"\t"+str(scores[11])+"\t"+
-                          str(scores[12])+"\t"+str(scores[13])+"\t"+str(scores[14])+"\t"+str(scores[15])+"\t"+
-                          str(scores[16])+"\t"+str(scores[17])+"\t"+str(scores[18])+"\t"+str(scores[19])+"\n")
+                file.write(str(scores[0])+“\t”+str(scores[1])+“\t”+str(scores[2])+“\t”+str(scores[3])+“\t”+
+                          str(scores[4])+“\t”+str(scores[5])+“\t”+str(scores[6])+“\t”+str(scores[7])+“\t”+
+                          str(scores[8])+“\t”+str(scores[9])+“\t”+str(scores[10])+“\t”+str(scores[11])+“\t”+
+                          str(scores[12])+“\t”+str(scores[13])+“\t”+str(scores[14])+“\t”+str(scores[15])+“\t”+
+                          str(scores[16])+“\t”+str(scores[17])+“\t”+str(scores[18])+“\t”+str(scores[19])+“\n”)
 
     to_psi_blast_file(w_matrix, file_name)
 
@@ -183,39 +184,39 @@ def construct_pssm(data):
 
 def plot_losses(valid_loss,train_loss,burn_in=20):
     plt.figure(figsize=(15,4))
-    plt.plot(list(range(burn_in, len(train_loss))), train_loss[burn_in:], label='Training loss')
-    plt.plot(list(range(burn_in, len(valid_loss))), valid_loss[burn_in:], label='Validation loss')
+    plt.plot(list(range(burn_in, len(train_loss))), train_loss[burn_in:], label=‘Training loss’)
+    plt.plot(list(range(burn_in, len(valid_loss))), valid_loss[burn_in:], label=‘Validation loss’)
 
     # find position of lowest validation loss
     minposs = valid_loss.index(min(valid_loss))+1
-    plt.axvline(minposs, linestyle='--', color='r',label='Minimum Validation Loss')
+    plt.axvline(minposs, linestyle=‘--’, color=‘r’,label=‘Minimum Validation Loss’)
 
     plt.legend(frameon=False)
-    plt.xlabel('Epochs')
-    plt.ylabel('Loss')
+    plt.xlabel(‘Epochs’)
+    plt.ylabel(‘Loss’)
     plt.show()
 
 def plot_roc_curve(fpr,tpr,roc_auc,peptide_length=[9]):
-    plt.title('Receiver Operating Characteristic')
-    plt.plot(fpr, tpr, label = 'AUC = %0.2f (%smer)' %(roc_auc, '-'.join([str(i) for i in peptide_length])))
-    plt.legend(loc = 'lower right')
-    plt.plot([0, 1], [0, 1], c='black', linestyle='--')
-    plt.ylabel('True Positive Rate')
-    plt.xlabel('False Positive Rate')
+    plt.title(‘Receiver Operating Characteristic’)
+    plt.plot(fpr, tpr, label = ‘AUC = %0.2f (%smer)’ %(roc_auc, ‘-’.join([str(i) for i in peptide_length])))
+    plt.legend(loc = ‘lower right’)
+    plt.plot([0, 1], [0, 1], c=‘black’, linestyle=‘--’)
+    plt.ylabel(‘True Positive Rate’)
+    plt.xlabel(‘False Positive Rate’)
 
 def plot_mcc(y_test,pred,mcc):
-    plt.title('Matthews Correlation Coefficient')
-    plt.scatter(y_test.flatten().detach().numpy(), pred.flatten().detach().numpy(), label = 'MCC = %0.2f' % mcc)
-    plt.legend(loc = 'lower right')
-    plt.ylabel('Predicted')
-    plt.xlabel('Validation targets')
+    plt.title(‘Matthews Correlation Coefficient’)
+    plt.scatter(y_test.flatten().detach().numpy(), pred.flatten().detach().numpy(), label = ‘MCC = %0.2f’ % mcc)
+    plt.legend(loc = ‘lower right’)
+    plt.ylabel(‘Predicted’)
+    plt.xlabel(‘Validation targets’)
     plt.show()
 
 #from pytorchTools
 class EarlyStopping:
-    """Early stops the training if validation loss doesn't improve after a given patience."""
-    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt'):
-        """
+    “”"Early stops the training if validation loss doesn’t improve after a given patience.“”"
+    def __init__(self, patience=7, verbose=False, delta=0, path=‘checkpoint.pt’):
+        “”"
         Args:
             patience (int): How long to wait after last time validation loss improved.
                             Default: 7
@@ -224,8 +225,8 @@ class EarlyStopping:
             delta (float): Minimum change in the monitored quantity to qualify as an improvement.
                             Default: 0
             path (str): Path for the checkpoint to be saved to.
-                            Default: 'checkpoint.pt'
-        """
+                            Default: ‘checkpoint.pt’
+        “”"
         self.patience = patience
         self.verbose = verbose
         self.counter = 0
@@ -244,7 +245,7 @@ class EarlyStopping:
             self.save_checkpoint(val_loss, model)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            #print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            #print(f’EarlyStopping counter: {self.counter} out of {self.patience}‘)
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
@@ -253,9 +254,8 @@ class EarlyStopping:
             self.counter = 0
 
     def save_checkpoint(self, val_loss, model):
-        '''Saves model when validation loss decrease.'''
+        ‘’'Saves model when validation loss decrease.‘’'
         if self.verbose:
-            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+            print(f’Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...’)
         torch.save(model.state_dict(), self.path)
         self.val_loss_min = val_loss
-
--- scripts/model.py
+++ scripts/model.py
@@ -1,4 +1,3 @@
-
 #this has been taken from Paolos link
 
 import glob
@@ -12,10 +11,9 @@ from sklearn.metrics import roc_curve, confusion_matrix
 import torch
 import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
 import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
-import torch.nn.functional as F  # All functions that don't have any parameters
+import torch.nn.functional as F  # All functions that don’t have any parameters
 from sklearn.metrics import accuracy_score
 
-# ML architecture
 
 class Net(nn.Module):
     num_classes = 1
@@ -45,4 +43,41 @@ class Net(nn.Module):
         x = x.view(x.size(0), -1)
         x = torch.sigmoid(self.fc1(x))
 
+        return x
+
+
+class Net_th(nn.Module):
+    def __init__(self,  num_classes):
+        super(Net_th, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features)
+        self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=100, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1_bn = nn.BatchNorm1d(100)
+
+        self.conv2 = nn.Conv1d(in_channels=100, out_channels=100, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(100)
+
+        self.rnn = nn.LSTM(input_size=100,hidden_size=26,num_layers=3, dropout=0.1, batch_first=True, bidirectional = True)
+        self.drop = nn.Dropout(p = 0.1)
+
+        self.fc1 = nn.Linear(26*2, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.drop(x)
+        x = x.transpose_(2, 1)
+        x, (h, c) = self.rnn(x)
+        cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.drop(cat)
+        x = self.fc1(cat)
         return x
