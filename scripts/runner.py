e377df763c545e6d2a530f520befa15d069ce111
dayadmg <daya.dmg@gmail.com>
2021-11-22 15:29:08 +0100

    change in main2 and runner

--- scripts/runner.py
+++ scripts/runner.py
@@ -14,10 +14,10 @@ except:
     print(NBDIR + 'directory already exists')
 
 #hyperparameters
-numHN = [32]  #, 64, 128, 256]
-numFilter = [50] #, 100, 200]
-embeddings = ["blosum"]#  'Baseline', 'esm-1b',  , 'vhse'
-
+numHN = [32, 64, 128, 256] #
+numFilter = [50, 100, 200]
+embeddings = ["blosum", 'Baseline', 'esm-1b','vhse' ,"esm_ASM"]
+keep_energy=True
 
 #dropOutRate = 0.1  #Dayana
 #dropOutRate = 0.2 #Gul
@@ -31,5 +31,9 @@ for emb in embeddings:
 
             print('Running Notebook for {}'.format(emb))
             pm.execute_notebook(input_path='main2.ipynb',
-                                parameters={'embedding': emb, 'numHN':nn, 'numFilter':nf, 'dropOutRate': dropOutRate},
-                                output_path=NBDIR + '{}_{}_main2_encoding_{}_numHN_{}_filters_{}_dr_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(dropOutRate*10))))
+                                parameters={'embedding': emb,
+                                            'numHN':nn,
+                                            'numFilter':nf,
+                                            'dropOutRate': dropOutRate,
+                                            'keep_energy': keep_energy},
+                                output_path=NBDIR + '{}_{}_main2_encoding_{}_numHN_{}_filters_{}_dr_{}_keep_energy_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(dropOutRate*10)),keep_energy))
