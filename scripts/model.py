df6995cfb7591e52f65382a1552a8fc005f6d800
dayadmg <daya.dmg@gmail.com>
2021-11-22 15:00:13 +0100

    new runner, added net_thesis again, changes to main2

--- scripts/model.py
+++ scripts/model.py
@@ -4,6 +4,7 @@ import glob
 
 import pandas as pd
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.metrics import f1_score, accuracy_score
@@ -67,6 +68,42 @@ class Net_th(nn.Module):
 
         self.rnn = nn.LSTM(input_size=100,hidden_size=26,num_layers=3, dropout=0.1, batch_first=True, bidirectional = True)
         self.drop = nn.Dropout(p = 0.1)
+
+        self.fc1 = nn.Linear(26*2, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.drop(x)
+        x = x.transpose_(2, 1)
+        x, (h, c) = self.rnn(x)
+        cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.drop(cat)
+        x = self.fc1(cat)
+        return x
+
+class Net_project(nn.Module):
+    def __init__(self,  num_classes, n_features, numHN, numFilter, dropOutRate):
+        super(Net_th, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features)
+        self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1_bn = nn.BatchNorm1d(100)
+
+        self.conv2 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(100)
+
+        self.rnn = nn.LSTM(input_size=numFilter,hidden_size=numHN,num_layers=3, dropout=dropOutRate, batch_first=True, bidirectional = True)
+        self.drop = nn.Dropout(p = dropOutRate)
 
         self.fc1 = nn.Linear(26*2, num_classes)
         torch.nn.init.xavier_uniform_(self.fc1.weight)
--- scripts/runner.py
+++ scripts/runner.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 #initialize
 date = datetime.today().strftime('%Y%m%d')
+current_time = time.strftime("%H-%M-%S", time.localtime())
 NBDIR = 'notebooks/'
 
 try:
@@ -13,12 +14,12 @@ except:
     print(NBDIR + 'directory already exists')
 
 #hyperparameters
-numHN = [32, 64, 128, 256]
-numFilter = [50, 100, 200]
-embeddings = ['Baseline', 'esm-1b', "blosum", 'vhse']
-current_time = time.strftime("%H:%M:%S", time.localtime())
+numHN = [32]  #, 64, 128, 256]
+numFilter = [50] #, 100, 200]
+embeddings = ["blosum"]#  'Baseline', 'esm-1b',  , 'vhse'
 
-dropOutRate = 0.1  #Dayana
+
+#dropOutRate = 0.1  #Dayana
 #dropOutRate = 0.2 #Gul
 #dropOutRate = 0.3 #Enric
 #dropOutRate = 0.4 #Shannara
@@ -31,4 +32,4 @@ for emb in embeddings:
             print('Running Notebook for {}'.format(emb))
             pm.execute_notebook(input_path='main2.ipynb',
                                 parameters={'embedding': emb, 'numHN':nn, 'numFilter':nf, 'dropOutRate': dropOutRate},
-                                output_path=NBDIR + '{}_{}_main2_encoding_{}_numHN_{}_filters_{}_dr_{}.ipynb'.format(date, current_time, emb, nn, nf, dropOutRate))
+                                output_path=NBDIR + '{}_{}_main2_encoding_{}_numHN_{}_filters_{}_dr_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(dropOutRate*10))))
