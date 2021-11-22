f1c684319df4c1735f61b131af7135093a2b065e
gulsude <gulsudedemircan@gmail.com>
2021-11-22 12:50:11 +0100

    fixed seeds

--- scripts/model.py
+++ scripts/model.py
@@ -84,7 +84,7 @@ class Net_th(nn.Module):
 
 class Net_project(nn.Module):
     def __init__(self,  num_classes, n_features, numHN, numFilter,dropOutRate):
-        super(Net_th, self).__init__()
+        super(Net_project, self).__init__()
         self.bn0 = nn.BatchNorm1d(n_features)
         self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
         torch.nn.init.kaiming_uniform_(self.conv1.weight)
