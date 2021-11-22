60b05b1d4b8e48c2b147e43961272fd8e32834f8
dayadmg <daya.dmg@gmail.com>
2021-11-22 13:13:39 +0100

    new runner

--- scripts/runner.py
+++ scripts/runner.py
@@ -15,12 +15,20 @@ except:
 #hyperparameters
 numHN = [32, 64, 128, 256]
 numFilter = [50, 100, 200]
-dropOutRate = [0.1, 0.2, 0.3, 0.4, 0.5]
-embeddings = ['Baseline', 'esm-1b']
+embeddings = ['Baseline', 'esm-1b', "blosum", 'vhse']
 current_time = time.strftime("%H:%M:%S", time.localtime())
 
+dropOutRate = 0.1  #Dayana
+#dropOutRate = 0.2 #Gul
+#dropOutRate = 0.3 #Enric
+#dropOutRate = 0.4 #Shannara
+#dropOutRate = 0.5 #Huijiao
+
 for emb in embeddings:
-    print('Running Notebook for {}'.format(emb))
-    pm.execute_notebook(input_path='main2.ipynb',    
-                        parameters={'embedding': emb},
-                        output_path=NBDIR + '{}_{}_main2_encoding_{}.ipynb'.format(date, current_time, emb))
+    for nn in numHN:
+        for nf in numFilter:
+
+            print('Running Notebook for {}'.format(emb))
+            pm.execute_notebook(input_path='main2.ipynb',
+                                parameters={'embedding': emb, 'numHN':nn, 'numFilter':nf, 'dropOutRate': dropOutRate},
+                                output_path=NBDIR + '{}_{}_main2_encoding_{}_numHN_{}_filters_{}_dr_{}.ipynb'.format(date, current_time, emb, nn, nf, dropOutRate))
