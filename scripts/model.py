978f9f257d1cc91b4270707fd131f9513b934368
dayadmg <daya.dmg@gmail.com>
2021-12-05 12:06:01 +0100

    adding main 4

--- scripts/model.py
+++ scripts/model.py
@@ -124,3 +124,147 @@ class Net_project(nn.Module):
         cat = self.drop(cat)
         x = self.fc1(cat)
         return x
+
+class Net_project2(nn.Module):
+    def __init__(self,  num_classes, n_features, numHN, numFilter, dropOutRate):
+        super(Net_project, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features)
+        self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1_bn = nn.BatchNorm1d(numFilter)
+
+        self.conv2 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference added a conv layer
+        self.conv3 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference num_layer 3 > 6
+        self.rnn = nn.LSTM(input_size=numFilter,hidden_size=numHN,num_layers=6, dropout=dropOutRate, batch_first=True, bidirectional = True)
+        self.drop = nn.Dropout(p = dropOutRate)
+
+        self.fc1 = nn.Linear(numHN*2, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.drop(x)
+        x = self.conv3_bn(x)
+        x = self.drop(x)
+        x = x.transpose_(2, 1)
+        x, (h, c) = self.rnn(x)
+        cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.drop(cat)
+        x = self.fc1(cat)
+        return x
+
+class Net_project3(nn.Module):
+    def __init__(self,  num_classes, n_features, numHN, numFilter, dropOutRate):
+        super(Net_project, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features)
+        #change in kernel and stride
+        self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=numFilter, kernel_size=4, stride=3, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1_bn = nn.BatchNorm1d(numFilter)
+        # change in kernel and stride
+        self.conv2 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=4, stride=3, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference added a conv layer
+        self.conv3 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference num_layer 3 > 6
+        self.rnn = nn.LSTM(input_size=numFilter,hidden_size=numHN,num_layers=6, dropout=dropOutRate, batch_first=True, bidirectional = True)
+        self.drop = nn.Dropout(p = dropOutRate)
+
+        self.fc1 = nn.Linear(numHN*2, numHN)
+        self.relu = nn.ReLU()
+        self.ln_bn = nn.BatchNorm1d(numHN)
+        self.fc2 = nn.Linear(numHN, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.drop(x)
+        x = self.conv3_bn(x)
+        x = self.drop(x)
+        x = x.transpose_(2, 1)
+        x, (h, c) = self.rnn(x)
+        cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.drop(cat)
+        x = self.relu(self.fc1(cat))
+        x = self.ln_bn(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        return x
+
+class Net_project4(nn.Module):
+    def __init__(self,  num_classes, n_features, numHN, numFilter, dropOutRate):
+        super(Net_project, self).__init__()
+        self.bn0 = nn.BatchNorm1d(n_features)
+
+        self.conv1 = nn.Conv1d(in_channels=n_features, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv1.weight)
+        self.pool = nn.MaxPool1d(kernel_size=2, stride=2)
+        self.conv1_bn = nn.BatchNorm1d(numFilter)
+
+        self.conv2 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference added a conv layer
+        self.conv3 = nn.Conv1d(in_channels=numFilter, out_channels=numFilter, kernel_size=3, stride=2, padding=1)
+        torch.nn.init.kaiming_uniform_(self.conv2.weight)
+        self.conv2_bn = nn.BatchNorm1d(numFilter)
+
+        #main difference num_layer 3 > 6
+        self.rnn = nn.LSTM(input_size=numFilter,hidden_size=numHN,num_layers=6, dropout=dropOutRate, batch_first=True, bidirectional = True)
+        self.drop = nn.Dropout(p = dropOutRate)
+
+        self.fc1 = nn.Linear(numHN*2, numHN)
+        self.relu = nn.ReLU()
+        self.ln_bn = nn.BatchNorm1d(numHN)
+        self.fc2 = nn.Linear(numHN, num_classes)
+        torch.nn.init.xavier_uniform_(self.fc1.weight)
+
+
+    def forward(self, x):
+        x = self.bn0(x)
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.conv1_bn(x)
+        x = self.drop(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.conv2_bn(x)
+        x = self.drop(x)
+        x = self.conv3_bn(x)
+        x = self.drop(x)
+        x = x.transpose_(2, 1)
+        x, (h, c) = self.rnn(x)
+        cat = torch.cat((h[-2, :, :], h[-1, :, :]), dim=1)
+        cat = self.drop(cat)
+        x = self.relu(self.fc1(cat))
+        x = self.ln_bn(x)
+        x = self.drop(x)
+        x = self.fc2(x)
+        return x
