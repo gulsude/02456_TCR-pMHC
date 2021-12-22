a6bf57c8da4aeba4ed1e89574019dc7f16760428
dayadmg <daya.dmg@gmail.com>
2021-12-22 19:13:47 +0100

    new runnerROC

--- scripts/runnerROC.py
+++ scripts/runnerROC.py
@@ -56,120 +56,12 @@ for sep in esm_1b_separated:
 
 
 #hyperparameters
-sm_1b_separated = [False]  ### upd
-embedding = ['esm_ASM']
-numHN = [26]
-numFilter = [200]
-learning_rate=[0.001]
-weight_decay = [ 0.0005]
-dropOutRate = [0.1]
-
-
-for sep in esm_1b_separated:
-    for emb in embedding:
-        for nn in numHN:
-            for nf in numFilter:
-                for lr in learning_rate:
-                    for wd in weight_decay:
-                        for do in dropOutRate:
-                            if emb == 'esm_ASM' and sep == True:
-                                continue
-                            else:
-                                notebook_name = 'ROC_{}_{}_mainROC_encoding_{}_numHN_{}_filters_{}_dr_{}_lr_{}_wc_{}_separated_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(do*10)), str(lr).replace(".",""), str(wd).replace(".",""), sep)
-                                print("Running:", notebook_name)
-
-                                pm.execute_notebook(input_path='mainROC.ipynb',
-                                                    parameters={'embedding': emb,
-                                                                'numHN':nn,
-                                                                'numFilter': nf,
-                                                                'dropOutRate': do,
-                                                                'esm_1b_separated': sep,  ### upd
-                                                                'learning_rate': lr,
-                                                                'weight_decay': wd,
-                                                                'name_experiment': name_experiment
-                                                                },
-                                            output_path=NBDIR + notebook_name
-                                            )
-
-#hyperparameters
-esm_1b_separated = [True]  ### upd
-embedding = ['esm-1b']
-numHN = [26]
-numFilter = [100]
-learning_rate=[0.001]
-weight_decay = [ 0.0001]
-dropOutRate = [0.3]
-
-
-for sep in esm_1b_separated:
-    for emb in embedding:
-        for nn in numHN:
-            for nf in numFilter:
-                for lr in learning_rate:
-                    for wd in weight_decay:
-                        for do in dropOutRate:
-                            if emb == 'esm_ASM' and sep == True:
-                                continue
-                            else:
-                                notebook_name = 'ROC_{}_{}_mainROC_encoding_{}_numHN_{}_filters_{}_dr_{}_lr_{}_wc_{}_separated_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(do*10)), str(lr).replace(".",""), str(wd).replace(".",""), sep)
-                                print("Running:", notebook_name)
-
-                                pm.execute_notebook(input_path='mainROC.ipynb',
-                                                    parameters={'embedding': emb,
-                                                                'numHN':nn,
-                                                                'numFilter': nf,
-                                                                'dropOutRate': do,
-                                                                'esm_1b_separated': sep,  ### upd
-                                                                'learning_rate': lr,
-                                                                'weight_decay': wd,
-                                                                'name_experiment': name_experiment
-                                                                },
-                                            output_path=NBDIR + notebook_name
-                                            )
-
-#hyperparameters
-esm_1b_separated = [True]  ### upd
-embedding = ['esm_ASM']
-numHN = [26]
-numFilter = [200]
-learning_rate=[0.001]
-weight_decay = [ 0.0005]
-dropOutRate = [0.1]
-
-
-for sep in esm_1b_separated:
-    for emb in embedding:
-        for nn in numHN:
-            for nf in numFilter:
-                for lr in learning_rate:
-                    for wd in weight_decay:
-                        for do in dropOutRate:
-                            if emb == 'esm_ASM' and sep == True:
-                                continue
-                            else:
-                                notebook_name = 'ROC_{}_{}_mainROC_encoding_{}_numHN_{}_filters_{}_dr_{}_lr_{}_wc_{}_separated_{}.ipynb'.format(date, current_time, emb, nn, nf, (int(do*10)), str(lr).replace(".",""), str(wd).replace(".",""), sep)
-                                print("Running:", notebook_name)
-
-                                pm.execute_notebook(input_path='mainROC.ipynb',
-                                                    parameters={'embedding': emb,
-                                                                'numHN':nn,
-                                                                'numFilter': nf,
-                                                                'dropOutRate': do,
-                                                                'esm_1b_separated': sep,  ### upd
-                                                                'learning_rate': lr,
-                                                                'weight_decay': wd,
-                                                                'name_experiment': name_experiment
-                                                                },
-                                            output_path=NBDIR + notebook_name
-                                            )
-
-#hyperparameters
 esm_1b_separated = [False]  ### upd
-embedding = ['Baseline']
+embedding = ['esm_ASM']
 numHN = [26]
-numFilter = [100]
+numFilter = [200]
 learning_rate=[0.001]
-weight_decay = [ 0.0001]
+weight_decay = [ 0.0005]
 dropOutRate = [0.1]
 
 
